5bc0603ea96afd85a17883a074154b56b00f07ac
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-05 13:23:48 +0100

    Test printing bits

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:59:32 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/04 18:25:30 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/09/05 13:21:17 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ int	main(int ac, char **av)
 {
 	int     pid;
         int     status;
+        // int     bit;
+        int     i;
+        int     j;
 
         if (ac != 3)
         {
@@ -25,10 +28,20 @@ int	main(int ac, char **av)
         }
 	pid = atoi(av[1]);
 	printf("PID SERVER: %d \n", pid);
-        status = kill(pid, 11);
+        status = kill(pid, 23);
         if (!status)
         {
-                printf("Success");
+                printf("Success\n");
+                i = 0;
+                j = 8;
+                while (av[2][i])
+                {
+                        while (j--)
+                        {
+                                printf("%c", ((av[2][i] >> j & 1) + '0'));
+                        }
+                        i++;
+                }
                 exit(0);
         }
         else
