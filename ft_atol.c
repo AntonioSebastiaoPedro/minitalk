9d7832d3cbfd2ebd12fa2603757497e6f92594bd
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-07 07:06:23 +0100

    Function to convert string to long

---
+++ ft_atol.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 11:27:45 by ansebast          #+#    #+#             */
+/*   Updated: 2024/09/07 00:34:27 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	int		signal;
+        long	num;
+
+	signal = 1;
+	num = 0;
+	i = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		signal = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		num = num * 10 + str[i] - '0';
+		i++;
+	}
+	return (num * signal);
+}
