16f9511b3483ea71360769f77d895f833723bb0c
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-07 08:07:34 +0100

    Handler Ctrl+C

--- server.c
+++ server.c
@@ -6,43 +6,50 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:59:44 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/07 06:51:46 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/09/07 07:52:08 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long     client_pid;
+long	client_pid;
+
+void    hand_quit(int signal)
+{
+        (void)signal;
+        ft_putstr_fd("\n\nYou shut down the server\n", 1);
+        exit(0);
+}
 
 void	hand_siguser(int sig, siginfo_t *info, void *context)
 {
 	static char	value;
 	static int	count;
-        
-        if (client_pid <= 0)
-                client_pid = info->si_pid;
-        if (client_pid == info->si_pid)
-        {
-                (void)context;
-                value *= 2;
-                if (sig == SIGUSR2)
-                        value++;
-                ++count;
-                if (count == 8)
-                {
-                        write(1, &value, 1);
-                        count = 0;
-                        if (value == '\0')
-                        {
-                                kill(client_pid, SIGUSR1);
-                                client_pid = -123;
-                        }
-                        value = 0;
-                }
-                kill(client_pid, SIGUSR1);
-        }
-        else
-                kill(info->si_pid, 9);
+
+	if (client_pid <= 0)
+		client_pid = info->si_pid;
+	if (client_pid == info->si_pid)
+	{
+		(void)context;
+		value *= 2;
+		if (sig == SIGUSR2)
+			value++;
+		++count;
+		if (count == 8)
+		{
+			write(1, &value, 1);
+			count = 0;
+			if (value == '\0')
+			{
+				kill(client_pid, SIGUSR1);
+				client_pid = -123;
+			}
+			value = 0;
+		}
+		kill(client_pid, SIGUSR1);
+	}
+	else
+		kill(info->si_pid, 9);
 }
 
 int	main(void)
@@ -60,6 +67,7 @@ int	main(void)
 
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
+        signal(SIGINT, hand_quit);
 
 	while (1)
 		pause();
