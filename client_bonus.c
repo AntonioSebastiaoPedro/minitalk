ee588b16663bd695ec75b099641adead5bc09e9c
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-12 22:19:38 +0100

    Print message after complete

--- client_bonus.c
+++ client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:59:32 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/12 08:28:49 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/09/12 22:18:39 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,18 @@
 
 int		g_signal_received;
 
-void	wait_signal(int signal)
-{
-	if (signal == SIGUSR1)
-		g_signal_received = 1;
-        else
-                g_signal_received = 0;
-}
-
 void    message_received(void)
 {
-        ft_putstr_fd("Message Send Successfuly!", 1);
+        ft_putstr_fd("Message Sent Successfuly!\n", 1);
         exit(0);
 }
 
-void	wait_signal_2(int signal)
+void	wait_signal(int signal)
 {
-        if (signal == 12)
-                message_received();
-        (void)signal;
-        g_signal_received = 2;
+	if (signal == 10)
+		g_signal_received = 1;
+        else if (signal == 12)
+		message_received();
 }
 
 static void	send_signal(long pid, unsigned char bit)
@@ -44,11 +36,6 @@ static void	send_signal(long pid, unsigned char bit)
 		kill(pid, SIGUSR2);
 	while (g_signal_received == 0)
 		;
-        if (g_signal_received == 2)
-        {
-                ft_puterror("ffdd", 2);
-                message_received();
-        }
 	g_signal_received = 0;
 }
 
@@ -75,13 +62,14 @@ int	main(int ac, char **av)
 	i = 0;
 	j = 8;
 	signal(SIGUSR1, wait_signal);
-        signal(SIGUSR2, wait_signal_2);
+        signal(SIGUSR2, wait_signal);
 	while (av[2][i] != '\0')
 	{
 		send_bits(j, av[2][i], pid, send_signal);
 		j = 8;
 		i++;
 	}
-	send_bits(j, av[2][i], pid, send_signal);
+        send_bits(j, '\n', pid, send_signal);
+        send_bits(j, av[2][i], pid, send_signal);
 	return (0);
 }
